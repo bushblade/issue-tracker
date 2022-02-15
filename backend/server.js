251d92ba064d19ab6f27023247562b5a5b87fe9f
Hrishabh Goel <hrishabh.w21@gmail.com>
2022-02-15 04:33:56 -0800

    first commit/backend main  work done

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,28 @@
-console.log('server');
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+
+// Custom Modules
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+
+// Connect to DataBase
+connectDB();
+
+const PORT = process.env.PORT || 5002;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.json({});
+});
+
+// ROUTES
+app.use('/api/users', require('./routes/userRoutes'));
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`app is listenig on PORT ${PORT}`);
+});
